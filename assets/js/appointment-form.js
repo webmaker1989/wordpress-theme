3460bfb3ce716ae70cff9fde9e45d02b8441b494
Sarbjeet Singh <sabi521@gmail.com>
2023-10-07 11:13:06 -0400

    file update

--- assets/js/appointment-form.js
+++ assets/js/appointment-form.js
@@ -21,22 +21,79 @@ function submit_data(e){
 
   //console.log(formdata);
 
-  const options = {
-    method: 'POST',
-    body: formData // Use FormData as the request body
-};
-
 
 const url = `${form_val.url}?action=get_appointment`;
 
-fetch(url, options)
+fetch(url , {
+    method: 'POST',
+    body: formData // Use FormData as the request body
+})
 .then(response => response.json())
+
 .then(response => {
     // Handle the response.
     console.log(response);
 })
+
 .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
     // Handle errors here
 });
 }
+
+
+
+
+
+/***
+ * 
+ */
+
+/*
+console.log("appointment confirm");
+console.log(form_val.url);
+document.getElementById("appointment-btn").addEventListener('click', submit_data);
+
+function submit_data(e){
+    e.preventDefault();
+
+    let form = document.getElementById('appointment');
+
+    let firstname = form.elements.firstname.value;
+    let lastname = form.elements.lastname.value;
+    let msg = form.elements.msg.value;
+
+    // Create a JavaScript object to represent the form data
+    const formData = {
+        fname: firstname,
+        lname: lastname,
+        msg: msg
+    };
+
+    // Convert the object to JSON
+    const formDataJSON = JSON.stringify(formData);
+
+    const url = `${form_val.url}?action=get_appointment`;
+
+    const options = {
+        method: 'POST',
+        body: formDataJSON, // Use JSON as the request body
+        headers: {
+            'Content-Type': 'application/json' // Specify that you are sending JSON data
+        }
+    };
+
+    fetch(url, options)
+    .then(response => response.json())
+    .then(response => {
+        // Handle the response.
+        console.log(response);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        // Handle errors here
+    });
+}
+
+
+*/
